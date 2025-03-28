2329c19e525557a76e2efdd7743c08f5886d5653
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 09:08:26 +0800

    adding zeros before the number input

--- batch_5/print_6_digit_character_input.py
+++ batch_5/print_6_digit_character_input.py
@@ -1,7 +1,11 @@
 # Ask the user to input a number from 0-1000
 num_input = input("Enter a number (0-1000): ")
 # Get the length of the inputed number
+input_length = len(num_input)
 # Subtract it from 6
+no_of_zeros = 6 - input_length
 # Multiply "0" and the difference of the 6 and length
+zeros = "0" * no_of_zeros
 # Add the zeros before the inputted number
+new_number = zeros + num_input
 # Print the modified number
