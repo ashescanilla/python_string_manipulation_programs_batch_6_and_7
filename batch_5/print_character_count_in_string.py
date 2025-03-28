b5035bf04dcb1f6d1b06224a43dbb1d01632b15b
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 09:27:54 +0800

    getting character count of input

--- batch_5/print_character_count_in_string.py
+++ batch_5/print_character_count_in_string.py
@@ -1,4 +1,5 @@
 # Ask the user to input their full name
 full_name = input("Enter you full name: ")
 # Count the number of characters using the 'len()' method
+character_count = len(full_name)
 # Print the number of characters
