11c3f9af875b1299890301eb5a4e522ed2701daa
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 14:24:42 +0800

    created pseudocode

---
+++ batch_6/print_check_uppercase_without_isupper.py
@@ -0,0 +1,5 @@
+# Get user input
+# Initialize a flag variable
+# Iterate through each character in the string
+    # Check if the character is a letter and not uppercase using ASCII values
+# Print the result
