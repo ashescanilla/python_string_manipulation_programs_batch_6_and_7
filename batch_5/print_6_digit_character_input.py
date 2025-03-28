03f88951d5d2e9a9bc210cfa4039d1e65ca3fbd9
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 09:07:11 +0800

    number input added

--- batch_5/print_6_digit_character_input.py
+++ batch_5/print_6_digit_character_input.py
@@ -1,4 +1,5 @@
 # Ask the user to input a number from 0-1000
+num_input = input("Enter a number (0-1000): ")
 # Get the length of the inputed number
 # Subtract it from 6
 # Multiply "0" and the difference of the 6 and length
