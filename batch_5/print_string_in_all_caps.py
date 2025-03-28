3435e42d9a6946c76901dd0dd3665574bf116372
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 10:02:56 +0800

    printing modified user input

--- batch_5/print_string_in_all_caps.py
+++ batch_5/print_string_in_all_caps.py
@@ -3,3 +3,4 @@ full_name = input("Enter your full name: ")
 # Modify the string using the 'upper()' method to convert it to all capital letters
 full_name = full_name.upper()
 # Print the modified string
+print(full_name)
