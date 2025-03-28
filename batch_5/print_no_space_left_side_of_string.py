fa35aed2ff2264bda99f4012a14b5e49eabb3f46
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 09:33:00 +0800

    printing the modified string

--- batch_5/print_no_space_left_side_of_string.py
+++ batch_5/print_no_space_left_side_of_string.py
@@ -3,3 +3,4 @@ full_name = input("Enter your full name: ")
 # Modify the input using the 'lstrip()' method with an empty space inside quotations as parameters
 full_name = full_name.lstrip(" ")
 # Print the modified string
+print(full_name)
