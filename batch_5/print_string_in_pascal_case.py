a25940b7714dd3e5b2328216a834b5f78110a65f
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 10:12:45 +0800

    modifying input to pascal case

--- batch_5/print_string_in_pascal_case.py
+++ batch_5/print_string_in_pascal_case.py
@@ -1,5 +1,6 @@
 # Ask the user to enter their name
 full_name = input("Please enter your full name: ")
 # Use the 'title()' method to capitalize the first letter of each word in the name
+full_name = full_name.title()
 # Use the 'replace()' method to replace the spaces to empty string
 # Print the formatted name
