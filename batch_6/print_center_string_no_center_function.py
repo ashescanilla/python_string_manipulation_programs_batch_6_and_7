5e33295795e1d94182aec6c34352d23ad1347db0
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 16:03:38 +0800

    created pseudocode

---
+++ batch_6/print_center_string_no_center_function.py
@@ -0,0 +1,6 @@
+# Get user input
+# Check if the string needs centering
+    # Calculate spaces on both sides
+    # Add spaces to center the text
+    # If no centering is needed, keep the string as is
+# Print the result
