b1d1e0882e42f7b0e99225824ac0ace7d9d6ec53
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 12:23:34 +0800

    created pseudocode

---
+++ batch_6/print_remove_spaces_without_lstrip.py
@@ -0,0 +1,5 @@
+# Get user input
+# Initialize an index to keep track of the current character position
+# Use a while loop to find the first non-space character
+# Extract the string from the first non-space character
+# Print the result
