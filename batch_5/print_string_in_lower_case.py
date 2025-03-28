f88c122ffcea25385271c4fc85d2fff18121b5e9
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 10:09:53 +0800

    modifying the user input with lower() method

--- batch_5/print_string_in_lower_case.py
+++ batch_5/print_string_in_lower_case.py
@@ -1,4 +1,5 @@
 # Ask user for their full name
 full_name = input("Enter your full name: ")
 # Modify the input using 'lower()' method to convert it to lower case
+full_name = full_name.lower()
 # Print the modified input
