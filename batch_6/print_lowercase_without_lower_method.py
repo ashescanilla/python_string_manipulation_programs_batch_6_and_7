c7773bae2a51ce14d9090fcf1013dfb88b99ab1a
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 13:03:02 +0800

    add variable to store converted lowercase text

--- batch_6/print_lowercase_without_lower_method.py
+++ batch_6/print_lowercase_without_lower_method.py
@@ -1,6 +1,7 @@
 # Get user input
 text = input("Enter a string: ")
 # Initialize an empty result string
+lowercase_text = ""
 # Iterate through each character in the string
     # Determine if the character is an uppercase letter (A-Z)
         # Convert the character to lowercase using ASCII value adjustment
