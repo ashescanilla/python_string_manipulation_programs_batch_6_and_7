c81b5b50fe0c3101d0c6d096aaa63c14caedb100
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 14:27:05 +0800

    set initial state for uppercase check

--- batch_6/print_check_uppercase_without_isupper.py
+++ batch_6/print_check_uppercase_without_isupper.py
@@ -1,6 +1,7 @@
 # Get user input
 text = input("Enter a string: ")
 # Initialize a flag variable
+all_uppercase = True
 # Iterate through each character in the string
     # Check if the character is a letter and not uppercase using ASCII values
 # Print the result
