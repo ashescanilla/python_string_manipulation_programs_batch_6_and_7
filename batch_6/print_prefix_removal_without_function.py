a16b6baddcb90ee92723068138fbbc4f4b2e2a7b
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 12:45:11 +0800

    add input prompts for string and prefix removal

--- batch_6/print_prefix_removal_without_function.py
+++ batch_6/print_prefix_removal_without_function.py
@@ -1,4 +1,6 @@
 # Get user input
+text = input("Enter a string: ")
+prefix = input("Enter the prefix to remove: ")
 # Check if the string starts with the prefix
     # Remove the prefix using slicing
     # Return original string if no prefix match
