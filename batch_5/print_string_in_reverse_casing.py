692321b2eedc7b6d96ba8c94d4f45703346be63e
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 10:17:43 +0800

    printing modified input

--- batch_5/print_string_in_reverse_casing.py
+++ batch_5/print_string_in_reverse_casing.py
@@ -3,3 +3,4 @@ full_name = input("Enter your name in wrong capitalization: ")
 # Using the 'swapcase()' method reverse the casing of the input
 full_name = full_name.swapcase()
 # Print the modified string
+print(full_name)
