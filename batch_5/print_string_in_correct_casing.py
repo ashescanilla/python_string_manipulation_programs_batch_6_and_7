8b325b09337edc40b86fa9244633af7aa3871c21
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 10:04:44 +0800

    created pseudocode

---
+++ batch_5/print_string_in_correct_casing.py
@@ -0,0 +1,6 @@
+# Ask the user to input their name in incorrect casing
+# Use the 'split()' method to convert each part to each independent string and store it in a list
+# Initializing a new list for correct capitalization
+# Itterate through the list and use the 'capitalize()' method to convert the stings into their correct casing
+# Use the 'join()' method with an empty space to join the each string with spaces
+# Print the modified string
