e2ab2aa0957c52ee6dd0c3b402302a0da7e85e96
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 10:19:19 +0800

    full name input added

--- batch_5/print_string_in_snake_case.py
+++ batch_5/print_string_in_snake_case.py
@@ -1,4 +1,5 @@
 # Ask the user to enter their name
+full_name = input("Enter your full name: ")
 # Use the 'lower()' method to convert the string into all lower case
 # Use the 'replace()' method to replace the spaces with underscores
 # Print the modified string
