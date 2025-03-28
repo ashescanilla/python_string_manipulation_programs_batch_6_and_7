e1942c7d29d65c95f8946609abd94c40d045e907
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 10:01:31 +0800

    add user input for their full name

--- batch_5/print_string_in_all_caps.py
+++ batch_5/print_string_in_all_caps.py
@@ -1,3 +1,4 @@
 # Ask the user to input their full name
+full_name = input("Enter your full name: ")
 # Modify the string using the 'upper()' method to convert it to all capital letters
 # Print the modified string
