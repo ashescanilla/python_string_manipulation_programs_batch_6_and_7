2e74945dcb8836a9d44369ad8442e632a02793d5
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 09:55:15 +0800

    created pseudocode

---
+++ batch_5/print_num_of_words_in_string.py
@@ -0,0 +1,4 @@
+# Ask the user to input a statement
+# Use to the 'split()' method to separate each word
+# Get the number of elements in the list using the len() method
+# Print the number of elements as the number of words
