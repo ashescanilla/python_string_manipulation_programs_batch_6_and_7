cf45e023a09726d662e7818d7b9d15eba274a0b6
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 09:32:45 +0800

    stripping the spaces on the left side of the string

--- batch_5/print_no_space_left_side_of_string.py
+++ batch_5/print_no_space_left_side_of_string.py
@@ -1,4 +1,5 @@
 # Ask the user to input their full name
 full_name = input("Enter your full name: ")
 # Modify the input using the 'lstrip()' method with an empty space inside quotations as parameters
+full_name = full_name.lstrip(" ")
 # Print the modified string
