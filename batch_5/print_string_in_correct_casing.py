6af435bc5510639bb545ee3bd028595dc40f89ef
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 10:05:10 +0800

    user input added

--- batch_5/print_string_in_correct_casing.py
+++ batch_5/print_string_in_correct_casing.py
@@ -1,4 +1,5 @@
 # Ask the user to input their name in incorrect casing
+full_name = input("Enter your name in wrong capitalization: ")
 # Use the 'split()' method to convert each part to each independent string and store it in a list
 # Initializing a new list for correct capitalization
 # Itterate through the list and use the 'capitalize()' method to convert the stings into their correct casing
