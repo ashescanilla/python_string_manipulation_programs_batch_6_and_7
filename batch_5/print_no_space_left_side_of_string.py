85cf78f294be10e44b2d3ac97288d269e0fe3b76
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 09:32:25 +0800

    full name input added

--- batch_5/print_no_space_left_side_of_string.py
+++ batch_5/print_no_space_left_side_of_string.py
@@ -1,3 +1,4 @@
 # Ask the user to input their full name
+full_name = input("Enter your full name: ")
 # Modify the input using the 'lstrip()' method with an empty space inside quotations as parameters
 # Print the modified string
