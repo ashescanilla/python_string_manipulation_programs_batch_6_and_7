86356bdf23e564f11e42b7b39d6afe45dd850d48
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 09:06:11 +0800

    created pseudocode

---
+++ batch_5/print_6_digit_character_input.py
@@ -0,0 +1,6 @@
+# Ask the user to input a number from 0-1000
+# Get the length of the inputed number
+# Subtract it from 6
+# Multiply "0" and the difference of the 6 and length
+# Add the zeros before the inputted number
+# Print the modified number
