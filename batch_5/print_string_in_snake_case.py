b3ea75879a6a1e102327d601cbc036239035ec36
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 10:19:48 +0800

    modifying input to snake case

--- batch_5/print_string_in_snake_case.py
+++ batch_5/print_string_in_snake_case.py
@@ -1,5 +1,7 @@
 # Ask the user to enter their name
 full_name = input("Enter your full name: ")
 # Use the 'lower()' method to convert the string into all lower case
+full_name = full_name.lower()
 # Use the 'replace()' method to replace the spaces with underscores
+full_name = full_name.replace(" ", "_")
 # Print the modified string
