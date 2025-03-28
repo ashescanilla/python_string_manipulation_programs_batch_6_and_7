6605dadb36499a0e794775eb95c6865b917ceac0
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-28 09:55:47 +0800

    added statement input

--- batch_5/print_num_of_words_in_string.py
+++ batch_5/print_num_of_words_in_string.py
@@ -1,4 +1,5 @@
 # Ask the user to input a statement
+statement = input("Please enter a statement: ")
 # Use to the 'split()' method to separate each word
 # Get the number of elements in the list using the len() method
 # Print the number of elements as the number of words
