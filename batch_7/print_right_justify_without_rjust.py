cb4458e78253d3d02b05540e169b57e88ef08583
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-31 12:54:46 +0800

    prompt user for desired width input​

--- batch_7/print_right_justify_without_rjust.py
+++ batch_7/print_right_justify_without_rjust.py
@@ -10,4 +10,5 @@ def print_right_justify_without_rjust(text, width):
 # Prompt the user to enter a string
 user_input = input("Enter a string: ")
 # Prompt the user to enter the desired width for right-justification
+width_input = int(input("Enter the desired width: "))
 # Call the function to right-justify the input string without using the rjust() method
