a248997e8ed94debcdc4514d572942c76b85fe40
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-31 17:24:42 +0800

    iterate over string indices using range(len(text))

--- batch_7/print_first_occurrence_without_index_function.py
+++ batch_7/print_first_occurrence_without_index_function.py
@@ -1,6 +1,7 @@
 # Function to find the first occurrence of a character without using index()
 def print_first_occurrence_without_index_function(text, char_to_find):
     # Iterate through each character in the string using its index
+    for i in range(len(text)):
         # Check if the current character matches the specified character
     # If the character is not found, display a message
 # Prompt the user to enter a string
