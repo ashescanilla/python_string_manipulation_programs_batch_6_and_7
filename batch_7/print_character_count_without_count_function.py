50116d527d2f709da21553043ab5e240d219de36
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-31 16:51:44 +0800

    increment counter when current character matches target character.

--- batch_7/print_character_count_without_count_function.py
+++ batch_7/print_character_count_without_count_function.py
@@ -6,6 +6,8 @@ def print_character_count_without_count_function(text, char_to_count):
     for char in text:
         # Compare the current character with the specified character
         if char == char_to_count:
+            # If they match, increment the counter
+            count += 1
     # Display the result
 # Prompt the user to enter a string
 # Prompt the user to enter the character whose occurrences are to be counted
