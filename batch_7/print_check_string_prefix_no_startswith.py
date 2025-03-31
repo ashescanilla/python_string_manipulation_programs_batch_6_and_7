312ed0d41a25f376ad77f3ed32a2f7ba6da97d8e
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-31 11:32:17 +0800

    prompt user for prefix input

--- batch_7/print_check_string_prefix_no_startswith.py
+++ batch_7/print_check_string_prefix_no_startswith.py
@@ -14,4 +14,5 @@ def print_string_starts_with_no_startswith(text, prefix):
 # Prompt the user to enter a string
 user_input = input("Enter a string: ")
 # Prompt the user to enter the prefix to check
+prefix_input = input("Enter the prefix to check: ")
 # Call the function to check if the input string starts with the specified prefix
