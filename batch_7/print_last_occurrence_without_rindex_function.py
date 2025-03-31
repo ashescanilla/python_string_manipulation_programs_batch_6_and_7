a202f21027d546f29f524b649bf60a141376f5c1
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-31 17:44:19 +0800

    add function to locate the last occurrence of a character in a string without using rindex()

--- batch_7/print_last_occurrence_without_rindex_function.py
+++ batch_7/print_last_occurrence_without_rindex_function.py
@@ -1,4 +1,5 @@
 # Program to find the first location of a character starting from the end without using rindex()
+def print_last_occurrence_without_rindex_function(text, char_to_find):
     # Iterate through the string in reverse using a negative step
         # Check if the current character matches the specified character
     # If the character is not found, display a message
