10e7d4633760dfefad3db2ba97da35b50787c8b6
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-31 16:46:05 +0800

    created pseudocode

---
+++ batch_7/print_character_count_without_count_function.py
@@ -0,0 +1,8 @@
+# Function to count character occurrences without using count()
+    # Initialize a counter to store the number of occurrences
+    # Iterate through each character in the string
+        # Compare the current character with the specified character
+    # Display the result
+# Prompt the user to enter a string
+# Prompt the user to enter the character whose occurrences are to be counted
+# Ensure only one character is provided for counting
